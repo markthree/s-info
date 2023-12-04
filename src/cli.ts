67c22f78449d2bb08339379865e2f256f2669cf2
markthree <1801982702@qq.com>
2023-12-04 14:29:55 +0800

    feat: add showProjectType

--- src/cli.ts
+++ src/cli.ts
@@ -1,9 +1,10 @@
 #!/usr/bin/env node
 
-import { separate, showRuntimeInfo } from "./index";
+import { separate, showProjectType, showRuntimeInfo } from "./index";
 
 async function runMain() {
   await separate(showRuntimeInfo);
+  await separate(showProjectType);
 }
 
 runMain();
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,9 @@
+import { existsSync } from "fs";
 import { version } from "process";
-import { createConsola } from "consola";
 import { lightGreen } from "kolorist";
+import { createConsola } from "consola";
 import { version as osVersion } from "os";
+import { join } from "path";
 
 const logger = createConsola().withTag("s-info");
 
@@ -10,6 +12,33 @@ export function showRuntimeInfo() {
   logger.log(
     `platform: ${lightGreen(osVersion())}`,
   );
+}
+
+export const CONFIG_FILE_SUFFIX = [
+  ".js",
+  ".mjs",
+  ".ts",
+  ".cjs",
+  ".mts",
+  ".cts",
+];
+
+export async function showProjectType() {
+  for (const SUFFIX of CONFIG_FILE_SUFFIX) {
+    if (existsSync(join("vite.config", SUFFIX))) {
+      logger.log(
+        `type: ${lightGreen("vite")}`,
+      );
+      return;
+    }
+
+    if (existsSync(join("nuxt.config", SUFFIX))) {
+      logger.log(
+        `type: ${lightGreen("nuxt")}`,
+      );
+      return;
+    }
+  }
 }
 
 export async function separate(fn: () => void) {
